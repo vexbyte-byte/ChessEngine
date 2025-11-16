facbaa7627314f02ba1dfa5f5808b948813d8dd0
Windows 11 22H2 <laserminigun1@gmail.com>
2025-11-16 12:21:47 +0800

    create file

---
+++ Shared.py
@@ -0,0 +1,11 @@
+# shared.py
+# This file handles shared variable across 3 other files
+# - Chess.py
+# - EngineHandler.py
+# - Engine.py
+
+# Variables
+# ---------
+current_board = {}   # your board dict
+last_user_move = None
+engine_result = None
