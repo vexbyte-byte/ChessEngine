047cbd5f8a9110a6aa51fbae0b3850a16f490376
Windows 11 22H2 <laserminigun1@gmail.com>
2025-11-18 17:38:26 +0800

    update file

--- shared.py
+++ shared.py
@@ -9,3 +9,25 @@
 current_board_arrangement = {}   # your board dict (dictionary)
 last_user_move = None
 engine_result = None
+
+castling_rights = {
+    # Initial
+    'white_kingside': True,
+    'white_queenside': True,
+    'black_kingside': True,
+    'black_queenside': True,
+
+    # king moved
+    'white_king_moved': False,
+    'black_king_moved': False,
+
+    # rook moved
+    # 'white_rook_1_moved': False,
+    # 'black_rook_1_moved': False,
+    # 'white_rook_2_moved': False,
+    # 'black_rook_2_moved': False,
+
+    # attacked coordinates
+    # 'white_kingside_attacked': [''],
+}
+
